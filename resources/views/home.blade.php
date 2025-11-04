f2f2c0a9f78964872470861ae78e7100e4fba989
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2025-11-04 15:48:48 +0800

    feat: update card body styling for improved readability

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                         </div>
                         <div class="col-md-2">
                             <div class="card bg-dark text-white">
-                                <div class="card-body text-center">
+                                <div class="card-body text-center text-white">
                                     <h6>Items Purchased</h6>
                                     <h3>{{ $totalPurchasedItems }}</h3>
                                 </div>
